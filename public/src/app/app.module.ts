3c33b620dd9d9d099d1cee72c7dc90d2d0b1bb6c
John Barrett <johnjbarrett22@gmail.com>
2019-06-03 02:46:17 -0700

    Created several components for site, awaiting response from client in terms of entire sitemap

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,11 +6,27 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpService } from './http.service';
 import { HomeComponent } from './home/home.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { ContactComponent } from './contact/contact.component';
+import { SitemapComponent } from './sitemap/sitemap.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { WhyComponent } from './why/why.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { WaterComponent } from './water/water.component';
+import { FireComponent } from './fire/fire.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FaqsComponent,
+    ContactComponent,
+    SitemapComponent,
+    PrivacyComponent,
+    WhyComponent,
+    InsuranceComponent,
+    WaterComponent,
+    FireComponent
   ],
   imports: [
     BrowserModule,
